d943675f2ca8f9098255bbef91585b7b4d7b1b27
Sadek <sade0027@stud.kea.dk>
2023-05-04 11:30:31 +0200

    Mainclass package created. main/base classes created.

--- src/main/java/com/example/alphasolutions/mainClasses/Employee.java
+++ src/main/java/com/example/alphasolutions/mainClasses/Employee.java
@@ -1,12 +1,21 @@
 package com.example.alphasolutions.mainClasses;
 
 public class Employee {
+
+
     int id;
     String name;
 
     public Employee(int id, String name){
         this.id = id;
         this.name = name;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
     }
 
 }
--- src/main/java/com/example/alphasolutions/mainClasses/SubTasks.java
+++ src/main/java/com/example/alphasolutions/mainClasses/SubTasks.java
@@ -1,15 +1,35 @@
 package com.example.alphasolutions.mainClasses;
 
 public class SubTasks {
-        private int id,cost, timeEstimate;
-        private String name;
 
-        public SubTasks(int id, String name,  int cost,int timeEstimate) {
-            this.id = id;
-            this.name = name;
-            this.cost = cost;
-            this.timeEstimate = timeEstimate;
-        }
 
+    private int id, cost, timeEstimate;
+    private String name;
+
+    public SubTasks(int id, String name, int cost, int timeEstimate) {
+        this.id = id;
+        this.name = name;
+        this.cost = cost;
+        this.timeEstimate = timeEstimate;
     }
-    // consider making it a subclass
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public int getTimeEstimate() {
+        return timeEstimate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
+// consider making it a subclass
+
+
