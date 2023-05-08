137dbf07c541a079aac7a7fb126cb94f483e00ad
toreh.simonsen@live.dk <tore0834@stud.kea.dk>
2023-05-08 14:40:00 +0200

    /as/ removed everywhere

--- src/main/java/com/example/alphasolutions/controller/AlphaSolutionsController.java
+++ src/main/java/com/example/alphasolutions/controller/AlphaSolutionsController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("as")
 public class AlphaSolutionsController {
     AlphaSolutionsService asService;
 
