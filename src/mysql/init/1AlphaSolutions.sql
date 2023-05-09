dec88c665a55e1b6e6cf38664053ce43453083ba
Sadek <sade0027@stud.kea.dk>
2023-05-09 09:26:25 +0200

    SQL scripts oprettet og tilføjet

--- src/mysql/init/1AlphaSolutions.sql
+++ src/mysql/init/1AlphaSolutions.sql
@@ -1,7 +1,46 @@
 CREATE database if not exists alphasolutions default character set utf8;
 use alphasolutions; 
-DROP table if exists names;
+DROP table if exists names, positions, employee,  WorksOn, project, tasks;
+
+
 CREATE table names (
 	nameString varchar (255), 
     nameID int primary key auto_increment
     );
+
+Create Table positions (
+  posID integer Primary key,
+  posName varchar(255)
+);
+
+
+CREATE Table employee (
+ empID integer auto_increment Primary key,
+  empName varchar (255),
+  positionID integer,
+  foreign key (positionID) REFERENCES positions(PosID)
+  );
+
+Create Table project (
+  projectID integer auto_increment primary key,
+  projectName varchar(255),
+  managerEmployee_ID integer,
+  foreign key (managerEmployee_ID) references employee(empID)
+ );
+ Create Table tasks (
+  taskID integer primary key auto_increment,
+  taskName varchar(255),
+  cost Integer,
+  totalEstimatedTime Integer,
+  project_ID integer,
+  superTask integer,
+  foreign key (project_ID) references project(projectID)
+);
+
+ 
+Create Table worksOn (
+  empID integer,
+  taskID integer,
+  foreign key (empID) REFERENCES employee(empID),
+  foreign key(taskID) REFERENCES tasks(taskID)
+);
