7dedfe603d102fa2d4879a7548adc0bf1d7854c9
Tohasim <toreh.simonsen@live.dk>
2023-05-13 11:35:22 +0200

    sub/supertasks working and showing now.

--- src/main/java/com/example/alphasolutions/DTOs/TasksDTO.java
+++ src/main/java/com/example/alphasolutions/DTOs/TasksDTO.java
@@ -1,21 +1,36 @@
 package com.example.alphasolutions.DTOs;
 
+import java.util.List;
+
 public class TasksDTO {
+    private int taskID;
     private String taskName;
     private String taskDescription;
     private int cost;
     private int totalEstimatedTime;
+    List<TasksDTO> subtasks;
 
+    public int getSuperTask() {
+        return superTask;
+    }
 
-    public TasksDTO(String taskName, String taskDescription, int cost, int totalEstimatedTime) {
+    public void setSuperTask(int superTask) {
+        this.superTask = superTask;
+    }
+
+    int superTask;
+
+    public TasksDTO(int taskID, String taskName, String taskDescription, int cost, int totalEstimatedTime, int superTask, List<TasksDTO> subtasks) {
+        this.taskID = taskID;
+        this.superTask = superTask;
         this.taskName = taskName;
         this.taskDescription = taskDescription;
         this.cost = cost;
         this.totalEstimatedTime = totalEstimatedTime;
+        this.subtasks = subtasks;
     }
 
     public TasksDTO() {
-
     }
 
     public String getTaskName() {
@@ -47,5 +62,27 @@ public class TasksDTO {
 
     public void setTotalEstimatedTime(int totalEstimatedTime) {
         this.totalEstimatedTime = totalEstimatedTime;
+    }
+
+
+    public int getTaskID() {
+        return taskID;
+    }
+
+    public void setTaskID(int taskID) {
+        this.taskID = taskID;
+    }
+
+    public List<TasksDTO> getSubtasks() {
+        return subtasks;
+    }
+
+    public void setSubtasks(List<TasksDTO> subtasks) {
+        this.subtasks = subtasks;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d: %s", taskID, taskName);
     }
 }
--- src/main/java/com/example/alphasolutions/repository/TaskRepository.java
+++ src/main/java/com/example/alphasolutions/repository/TaskRepository.java
@@ -16,15 +16,18 @@ public class TaskRepository {
         List<TasksDTO> tasks = new ArrayList<>();
         try {
             Connection con = DatabaseManager.getConnection();
-            String query = "Select taskName, taskDescription, cost, totalEstimatedTime from tasks"; // select columns
+            String query = "Select * from tasks"; // select columns
             PreparedStatement ps = con.prepareStatement(query);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
+                int taskID = rs.getInt("taskID");
                 String taskName = rs.getString("taskName");
                 String taskDescription = rs.getString("taskDescription");
                 int cost = rs.getInt("cost");
                 int totalEstimatedTime = rs.getInt("totalEstimatedTime");
-                tasks.add(new TasksDTO(taskName, taskDescription, cost, totalEstimatedTime)); // pass both columns to the constructor
+                int superTask = rs.getInt("superTask");
+                List<TasksDTO> subtasks = getSubtasks(rs.getInt("taskID"));
+                tasks.add(new TasksDTO(taskID, taskName, taskDescription, cost, totalEstimatedTime, superTask, subtasks)); // pass both columns to the constructor
                 //  rowCount++;
             }
             // System.out.println("Retrieved " + rowCount + " rows from employee table"); // print statement to indicate the number of rows retrieved
@@ -33,6 +36,30 @@ public class TaskRepository {
             throw new RuntimeException(e);
         }
         return tasks;
+    }
+
+    private List<TasksDTO> getSubtasks(int taskID) {
+        List<TasksDTO> subtasks = new ArrayList<>();
+        try{
+            Connection con = DatabaseManager.getConnection();
+            String query = "SELECT * FROM tasks WHERE superTask = ?";
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setInt(1, taskID);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                subtasks.add(new TasksDTO(
+                        rs.getInt("taskID"),
+                        rs.getString("taskName"),
+                        rs.getString("taskDescription"),
+                        rs.getInt("cost"),
+                        rs.getInt("totalEstimatedTime"),
+                        rs.getInt("superTask"),
+                        null));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return subtasks;
     }
 
     public void addTask(String taskName, String taskDescription, int cost, int totalEstimatedTime) {
