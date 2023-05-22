4dfadbeb3791cef28f6acd6ac25d1f4db2bbe25f
Sadek <sade0027@stud.kea.dk>
2023-05-22 12:12:26 +0200

    Comments added

--- src/main/java/com/example/alphasolutions/repository/TaskRepository.java
+++ src/main/java/com/example/alphasolutions/repository/TaskRepository.java
@@ -109,9 +109,11 @@ public class TaskRepository {
         }
         return task;
     }
+    // This method updates an existing task in the database with the given properties
     public void updateTask(TasksDTO updatedTask) {
         try {
             Connection con = DatabaseManager.getConnection();
+            // 'WHERE taskID = ?' specifies the condition to identify the specific task to update based on its taskID
             String query = "UPDATE tasks SET taskName = ?, taskDescription = ?, cost = ?, totalEstimatedTime = ?, superTask = ? WHERE taskID = ?";
             PreparedStatement ps = con.prepareStatement(query);
             ps.setString(1, updatedTask.getTaskName());
