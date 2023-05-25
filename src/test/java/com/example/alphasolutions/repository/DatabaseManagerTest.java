0396a012bc53b7cc62531bca6178ea0db1782f97
Tohasim <toreh.simonsen@live.dk>
2023-05-25 10:19:45 +0200

    DatabaseManager tested

---
+++ src/test/java/com/example/alphasolutions/repository/DatabaseManagerTest.java
@@ -0,0 +1,30 @@
+package com.example.alphasolutions.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DatabaseManagerTest {
+
+    private DatabaseManager databaseManager;
+
+    @BeforeEach
+    public void setup() {
+        databaseManager = new DatabaseManager();
+        databaseManager.setUrl("jdbc:mysql://localhost:3306/alphasolutions");
+        databaseManager.setUsername("root");
+        databaseManager.setPassword("root");
+    }
+
+    @Test
+    public void testGetConnection() throws SQLException {
+        Connection connection = databaseManager.getConnection();
+        assertNotNull(connection);
+        assertTrue(connection.isValid(5));
+    }
+}
